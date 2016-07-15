e5a4c58b897772fb9effc5cf5a72b58b35c79b35
Adam Gonzales <adamgonzales1@gmail.com>
2016-07-15 00:04:31 -0700

    update files

---
+++ chapter-08/js/index.js
@@ -0,0 +1,23 @@
+// extracting document info
+// function init() {
+// 	document.getElementById("panel");
+// 	panel.innerHTML += "Linked From: " + document.referrer;
+// 	panel.innerHTML += "<br>Title: " + document.title;
+// 	panel.innerHTML += "<br>URL: " + document.URL;
+// 	panel.innerHTML += "<br>Last Modified: " + document.lastModified;
+// }
+// document.addEventListener("DOMContentLoaded", init, false);
+
+// addressing component arrays
+// function init() {
+// 	var panel = document.getElementById("panel");
+// 	panel.innerHTML = "Document Components...";
+// 	panel.innerHTML += "<br>No. Forms: " + document.forms.length;
+// 	panel.innerHTML += "<br>No. Links: " + document.links.length;
+// 	panel.innerHTML += "<br>No. Anchors: " + document.anchors.length;
+// 	panel.innerHTML += "<br>No. Images: " + document.images.length;
+// 	panel.innerHTML += "<br>First Image URL: " + document.images[0].src;
+// }
+// document.addEventListener("DOMContentLoaded", init, false);
+
+// addressing components direct
