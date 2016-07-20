248cc53a4c9eff100cd3008d6fe92a6f81cc4cc9
Adam Gonzales <adamgonzales1@gmail.com>
2016-07-19 21:29:37 -0700

    chapter exercises

--- chapter-08/js/index.js
+++ chapter-08/js/index.js
@@ -21,3 +21,41 @@
 // document.addEventListener("DOMContentLoaded", init, false);
 
 // addressing components direct
+// function init() {
+// 	var panel = document.getElementById("panel");
+// 	var item1 = document.getElementById("item1");
+// 	var elems = document.getElementsByTagName("li");
+// 	var item2 = elems[4];
+// 	panel.innerHTML += "Total No. List Items: " + elems.length;
+// 	panel.innerHTML += 
+// 	"<br>Specific Item One: " + item1.innerHTML + 
+// 	"<br>Specific Item Two: " + item2.innerHTML;
+// }
+// document.addEventListener("DOMContentLoaded", init, false);
+
+// setting and retrieving cookies
+// function init() {
+// 	document.getElementById("panel");
+// 	var user = escape("Mike McGrath,000456");
+// 	var expiry = new Date();
+// 	expiry.setTime(expiry.getTime() + (7 * 24 * 60 * 60 * 1000));
+// 	document.cookie = "myData=" + user + ";" + "expires=" + expiry.toGMTString() + ";";
+	
+// 	if (document.cookie) {
+// 		var cookieString = unescape(document.cookie);
+// 		var list = cookieString.split("=");
+// 		if (list[0] === "myData") {
+// 			var data = list[1].split(",");
+// 			var userName = data[0];
+// 			var userAcct = data[1];
+// 		}
+// 	}
+
+// 	panel.innerHTML += "Cookie String: " + cookieString;
+// 	panel.innerHTML += "<br>Split List: " + list;
+// 	panel.innerHTML += "<br>User Name: " + userName;
+// 	panel.innerHTML += "<User Account: " + userAcct;
+
+// }
+// document.addEventListener("DOMContentLoaded", init, false);
+
