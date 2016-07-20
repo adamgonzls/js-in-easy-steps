718f9c384bff104b3766e82b33ec97244e0bc42d
Adam Gonzales <adamgonzales1@gmail.com>
2016-07-19 22:02:23 -0700

    focus lesson

--- chapter-09/js/index.js
+++ chapter-09/js/index.js
@@ -41,26 +41,90 @@
 // document.addEventListener("DOMContentLoaded", init, false);
 
 // acknowledging key strokes
-var panel;
+// var panel;
 
-function keydownResponse() {
-	panel.innerHTML += "<br>Key Pressed: ";
+// function keydownResponse() {
+// 	panel.innerHTML += "<br>Key Pressed: ";
+// }
+
+// function keyupResponse() {
+// 	panel.innerHTML += "<br>Key Released";
+// }
+
+// function keypressResponse(e) {
+// 	var keynum = (window.event) ? event.keyCode : e.which;
+// 	panel.innerHTML += String.fromCharCode(keynum);
+// }
+
+// function init() {
+// 	panel = document.getElementById("panel");
+// 	panel.innerHTML = "Press a key...<br>";
+// 	document.onkeydown = keydownResponse;
+// 	document.onkeyup = keyupResponse;
+// 	document.onkeypress = keypressResponse;
+// }
+// document.addEventListener("DOMContentLoaded", init, false);
+
+// var panel, flag;
+
+// function mousemoveResponse(e) {
+// 	var x, y;
+
+// 	if (window.event) {
+// 		x = event.x;
+// 		y = event.y;
+// 	} else if (e) {
+// 		x = e.pageX;
+// 		y = e.pageY;
+// 	}
+
+// 	if (flag) {
+// 		panel.innerHTML = "Mouse is at X: " + x + ", Y: " + y;
+// 	}
+// }
+
+// function mouseoverResponse() {
+// 	flag = false;
+// 	panel.innerHTML = "Mouse is over";
+// }
+
+// function mouseoutResponse() {
+// 	flag = true;
+// }
+
+// function init() {
+// 	panel = document.getElementById("panel");
+// 	flag = true;
+// 	panel.innerHTML = "Move the mouse...";
+// 	document.onmousemove = mousemoveResponse;
+// 	panel.onmouseover = mouseoverResponse;
+// 	panel.onmouseout = mouseoutResponse;
+// }
+// document.addEventListener("DOMContentLoaded", init, false);
+
+// indentifying focus
+function focusResponse() {
+	this.value = "In Focus";
 }
 
-function keyupResponse() {
-	panel.innerHTML += "<br>Key Released";
-}
-
-function keypressResponse(e) {
-	var keynum = (window.event) ? event.keyCode : e.which;
-	panel.innerHTML += String.fromCharCode(keynum);
+function blurResponse() {
+	this.value = "Lost Focus";
 }
 
 function init() {
-	panel = document.getElementById("panel");
-	panel.innerHTML = "Press a key...<br>";
-	document.onkeydown = keydownResponse;
-	document.onkeyup = keyupResponse;
-	document.onkeypress = keypressResponse;
+	var panel = document.getElementById("panel");
+	panel.innerHTML += "<input type='text' id='txt1'>";
+	panel.innerHTML += "<input type='text' id='txt2'>";
+	
+	var field1 = document.getElementById("txt1");
+	field1.onfocus = focusResponse;
+	field1.onblur = blurResponse;
+
+	var field2 = document.getElementById("txt2");
+	field2.onfocus = focusResponse;
+	field2.onblur = blurResponse;
+
+	field1.focus();
+
 }
 document.addEventListener("DOMContentLoaded", init, false);
