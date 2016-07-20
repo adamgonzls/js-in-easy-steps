ad4e0c4e34505c32ab3e581d95f0fdf34188643c
Adam Gonzales <adamgonzales1@gmail.com>
2016-07-19 22:52:51 -0700

    add chapter 10 files

---
+++ chapter-10/js/index.js
@@ -0,0 +1,20 @@
+function init() {
+	var panel = document.getElementById("panel");
+	panel.innerHTML += "Send Book Data";
+	
+	var form = document.getElementById("book");
+	form.action = "../echo.pl";
+	form.method = "POST";
+
+	var title = document.getElementById("title");
+	title.size = "30";
+	title.name = "Book Title";
+	title.value = "JavaScript in Easy Steps";
+
+	var author = document.getElementById("author");
+	author.size = "30";
+	author.name = "By Author";
+	author.value = "Mike McGrath";
+
+}
+document.addEventListener("DOMContentLoaded", init, false);
